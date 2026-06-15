dff2197332ab53664bd5122487d285b909613635
ramon_norabuena <diegora69@hotmail.com>
2026-06-14 19:33:32 -0500

    se agrega tipos de datos en python

---
+++ main.py
@@ -0,0 +1,37 @@
+
+########## Tipos de datos ##########
+
+# Enteros
+entero = 10
+
+# Flotantes
+flotante = 3.14
+
+# Cadenas de texto
+cadena = "Hola, mundo!"
+
+# booleanos
+booleano = True
+booleano2 = False
+
+# Listas
+lista = [ 1, 2, 3, 4, 5 ]  #mutable
+
+# Tuplas
+tupla = ( 1, 2, 3, 4, 5 )  #inmutable
+
+# Diccionarios
+diccionario = { "nombre": "Juan", "edad": 30, "ciudad": "Madrid" }  # llave-valor
+
+# Conjuntos
+conjunto = { 1, 2, 3, 4, 5 }  # No permite elementos duplicados y no tiene un orden específico
+
+print(entero)
+print(flotante)
+print(cadena)
+print(booleano)
+print(booleano2)
+print(lista)
+print(tupla)
+print(diccionario)
+print(conjunto)
